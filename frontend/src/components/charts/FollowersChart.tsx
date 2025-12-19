830d2ec94f40e2b291a097c7d8d5fb53b60fd666
ro7toz <tushkinit@gmail.com>
2025-12-20 00:02:35 +0530

    PUSH 2: All 12 Reusable Components - Header, Footer, Modals, Display Components, Charts

---
+++ frontend/src/components/charts/FollowersChart.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FollowersChart: React.FC = () => (
+  <div className="chart-container">
+    <h3>Followers Growth</h3>
+    <p>Chart implementation with Recharts</p>
+  </div>
+);
+
+export default FollowersChart;
