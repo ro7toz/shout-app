3071b66328df12a5bf392a0b4cfb6b590efd3cac
ro7toz <tushkinit@gmail.com>
2025-12-20 08:03:13 +0530

    Delete src/main/java/com/shoutx directory

--- src/main/java/com/shoutx/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.shoutx.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    // Web configuration can be added here
-}
